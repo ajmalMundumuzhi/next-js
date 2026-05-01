f424e186c27533be599ed106e7087d0f90e33f46
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-05-01 22:02:25 +0530

    routes creation adn navigation completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,8 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <Link href={"/"}>Home</Link>
+      <Link href={"/about"}>About</Link>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
