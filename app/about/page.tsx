436c9d9eecbff71760dd20cc5b225f08b51f247e
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-05-03 11:45:41 +0530

    metadata finsihed

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,34 @@
 import ButtonComponent from "./button";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Us | About The Company",
+  description: "What we are and what we do",
+  keywords: "about, company, mission, vision",
+   openGraph: {
+    title: "About Us | My Website",
+    description: "Learn more about our company, mission, and values.",
+    url: "https://hivlora.in/about",
+    siteName: "Hivlora",
+    images: [
+      {
+        url: "https://hivlora.in/og-image.jpg", // 👈 replace this
+        width: 1200,
+        height: 630,
+        alt: "About Us Image",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image", 
+    title: "About Us | My Website",
+    description: "Learn more about our company, mission, and values.",
+    images: ["https://yourdomain.com/og-image.jpg"], 
+  },
+};
+
 export default async function About () {
     console.log("Is this is a server or client component?")
     
